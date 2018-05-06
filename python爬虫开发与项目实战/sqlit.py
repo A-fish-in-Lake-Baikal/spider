985f8751ba16d9e3004e1f179da2f9371d3bde33
A-fish-in-Lake-Baikal <38976160+A-fish-in-Lake-Baikal@users.noreply.github.com>
2018-05-06 17:03:56 +0800

    爬虫入门练习
    2018.5.6

---
+++ python爬虫开发与项目实战/sqlit.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+cursor =conn.cursor()
+cursor.execute('create table user(id varchar(20) primary key,name varchar(20))')
+cursor.execute('insert into user(id, name) values (\'1\',\'Michael\')')
+cursor.rowcount
