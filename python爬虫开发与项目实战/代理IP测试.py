752de2926d8ab909f5a3e96fbd4c1a3dc99e8932
A-fish-in-Lake-Baikal <38976160+A-fish-in-Lake-Baikal@users.noreply.github.com>
2018-07-03 09:07:59 +0800

    代理IP测试
    代理ip测试

--- python爬虫开发与项目实战/代理IP测试.py
+++ python爬虫开发与项目实战/代理IP测试.py
@@ -1,5 +1,4 @@
 from urllib import request
-from bs4 import BeautifulSoup
 
 
 
@@ -7,17 +6,8 @@ def geturls(url,proxy_hander):
     opener = request.build_opener(proxy_hander)
     response = opener.open(url)
     print(response.read())
-    '''soup = BeautifulSoup(response,'html.parser')
-    a_list = soup.findAll('a')
-    return a_list'''
 
 if __name__=='__main__':
-    proxy = request.ProxyHandler({"http" : "http://222.219.168.188:4536"})
-    # url = 'http://news.sina.com.cn/'
+    proxy = request.ProxyHandler({"http" : "http://60.255.186.169:8888"})
     url = 'http://httpbin.org/get'
-    # urls = geturls(url,proxy_hander)
     geturls(url,proxy)
-    '''for newurl in urls:
-        if not newurl:
-            continue
-        print(newurl.get('href'))'''
