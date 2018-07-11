a6a4f05952ee375469a91a91bf93956a540bc24f
A-fish-in-Lake-Baikal <38976160+A-fish-in-Lake-Baikal@users.noreply.github.com>
2018-07-11 22:01:24 +0800

    beautfulsoup
    beautfulsoup

--- python爬虫开发与项目实战/beautifulsoup.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = 'http://www.baidu.com'
-response = requests.get(url)
-soup = BeautifulSoup(response.text,'html.parser')
-print(soup.prettify())
-print(soup.title.text)
-print(soup.body.attrs['link'])
