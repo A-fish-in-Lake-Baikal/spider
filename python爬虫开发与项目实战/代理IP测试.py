77ea61b853dfae6b7fc427085c92933e0a16804b
A-fish-in-Lake-Baikal <38976160+A-fish-in-Lake-Baikal@users.noreply.github.com>
2018-07-20 17:08:24 +0800

    代理ip
    代理ip

--- python爬虫开发与项目实战/代理IP测试.py
+++ python爬虫开发与项目实战/代理IP测试.py
@@ -1,13 +1,31 @@
 from urllib import request
+import requests
+import time
 
+PROXY_POOL_URL = 'http://192.168.1.63:5555/random'
+
+def get_proxy():
+    try:
+        response = requests.get(PROXY_POOL_URL)
+        if response.status_code == 200:
+            print("proxy-ip:"+response.text)
+            return response.text
+    except ConnectionError:
+        return None
 
 
 def geturls(url,proxy_hander):
+
     opener = request.build_opener(proxy_hander)
     response = opener.open(url)
     print(response.read())
 
 if __name__=='__main__':
-    proxy = request.ProxyHandler({"http" : "http://60.255.186.169:8888"})
+    time1 = time.time()
+    ip = get_proxy()
+    proxy = request.ProxyHandler({"http" : "http://"+ip})
     url = 'http://httpbin.org/get'
     geturls(url,proxy)
+    time2 = time.time()
+    times = time2-time1
+    print(times)
