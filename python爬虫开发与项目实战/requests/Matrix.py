5ea7cc3b5a5ab639817862cf0c8644a6c389e2ff
A-fish-in-Lake-Baikal <38976160+A-fish-in-Lake-Baikal@users.noreply.github.com>
2019-05-09 22:41:13 +0800

    123
    123

--- python爬虫开发与项目实战/requests/Matrix.py
+++ python爬虫开发与项目实战/requests/Matrix.py
@@ -4,36 +4,56 @@
 @time:2019/5/6 21:45
 '''
 import requests
+import sqlite3
+import time
 from bs4 import BeautifulSoup
 
 
 
 def getresponse(url):
+
+    datatime = time.strftime('%Y-%m-%d-%H-%M', time.localtime(time.time()))
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134'
     }
-    response = requests.get(url,headers=headers,timeout=1)
+    response = requests.get(url,headers=headers,timeout=5)
 
     try:
+        conn = sqlite3.connect('test.db')
+        c = conn.cursor()
         if response.status_code == 200:
             response.encoding = response.apparent_encoding
             html = response.text
             html = html.encode("utf-8")
-            # with open(r"./matrix.html","w+b") as f:
-            #     f.write(html)
-            # print(response.text)
             soup = BeautifulSoup(html,'lxml')
-            dls = soup.find_all("article")
-            print(dls)
-            # for dl in dls:
-            #     print(dl)
+            articles = soup.find_all("article")
+            for article in articles:
+                hs = article.find_all('header')
+                for h in hs:
+                    al = h.find_all(target='_blank')
+                    for a in al:
+                        sql = 'insert into xitmi(title,furl,times) values(?,?,?)'
+                        title = a.string
+                        vla = (title,url,datatime)
+                        c.execute(sql,vla)
+                        print('写入完成...')
+                        conn.commit()
+        conn.close()
+                        # print(a.string)
+                        # print(a.title)
+                    # h2s = h.find_all('h2')
+                    # for h2 in h2s:
+                    #     print(h2)
     except:
         return None
+
+# def write_sql(title,url):
 
 
 
 if __name__ == '__main__':
-    urls = ['https://www.xitmi.com/ask/page/{}'.format(str(i)) for i in range(1,15)]
+
+    urls = ['https://www.xitmi.com/page/{}'.format(str(i)) for i in range(1,10)]
     for url in urls:
         print(url)
         text = getresponse(url)
--- python爬虫开发与项目实战/sqlit.py
+++ python爬虫开发与项目实战/sqlit.py
@@ -11,11 +11,14 @@ datatime = time.strftime('%Y-%m-%d-%H-%M', time.localtime(time.time()))
 # )'''
 sql1 = 'insert into picture_url(picurl,datatime) values (?,?)'
 sql2 = 'insert into page_url(pageurl,datatime) values(?,?)'
+sql3 = 'insert into xitmi(title,furl,times) values(?,?,?)'
 vla1 = ('https://wx1.sinaimg.cn/mw600/46401622gy1fra33m4gd1j20u00gwjrx.jpg',datatime)
 vla2 = ('https://www.xitmi.com/1555.html',datatime)
+vla3 = ('CLaunch v3.29绿色汉化版快速启动软件附教程和皮肤','https://www. xi tmi. com/page/3',datatime)
 # c.execute(sql)
 c.execute(sql1,vla1)
 c.execute(sql2,vla2)
+c.execute(sql3,vla3)
 print(datatime)
 print('写入完成')
 conn.commit()
