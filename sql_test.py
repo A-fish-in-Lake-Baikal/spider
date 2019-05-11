d614ac5c8428d32a9bafd661cbd28258c72d34ad
A-fish-in-Lake-Baikal <38976160+A-fish-in-Lake-Baikal@users.noreply.github.com>
2019-05-11 13:43:24 +0800

    1234

--- sql_test.py
+++ sql_test.py
@@ -9,20 +9,7 @@ def sqlexecute():
     conn = pymssql.connect('192.168.2.142','sa','test12#$%','PSCR103')
     cursor = conn.cursor()
 
-    sql = """select top 100 mf.ResourceID, sf.UniqueID, mf.FileID, mf.Path from
-                    (select R.ID as ResourceID, F.UniqueID, F.ID as FileID, F.Path
-                        from
-                        PS_Resource R
-                        inner join PS_CategoryResource CR on CR.ResourceID = R.ID and R.IsDeleted = 0
-                        inner join PS_ResourceFile RF on RF.ResourceID = R.ID or R.ParentResourceID =R.ID
-                        inner join PS_File F on F.ID = RF.FileID and F.IsDeleted = 0
-                        where
-                        R.ResourceType = 3 and R.IsDeleted = 0 and RF.FileType = 1
-                        and f.ID not in (select FileID from ImageAnalysisFace)) mf
-                        join
-                        PS_ResourceFile rf on mf.FileID = rf.ParentFileID and rf.FileType=4
-                        join 
-                        PS_File sf on rf.FileID=sf.ID"""
+    sql = """select * from  PS_Resource"""
 
     cursor.execute(sql)
     for row in cursor:
